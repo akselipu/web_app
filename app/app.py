2fbbbff76fdc57ee10d7d320df936e7514fb0f5d
Akseli Puustinen <akseli.puustinen@gmail.com>
2024-09-19 15:26:41 +0300

    updated file app.py and datisbase.py

--- app/app.py
+++ app/app.py
@@ -5,8 +5,7 @@ from config import Config
 
 app = Flask(__name__)
 app.secret_key = "abc1234567890" 
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://leopuustinen:aksu@localhost:5432/postgres"
-#app.config.from_object(Config)
+
 
 # Import your routes after initializing the app and db
 import routes
